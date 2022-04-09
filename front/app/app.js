2fedb47e63c93ba718f23e0205084aa4aaa5113f
KanHatakeyama <yaj_k@msn.com>
2022-04-10 07:58:04 +0900

    add routing (not complete)

--- front/app/app.js
+++ front/app/app.js
@@ -1,11 +1,11 @@
 import Modeler from 'bpmn-js/lib/Modeler';
 import PropertiesPanel from './properties-panel';
 import customModdleExtension from './moddle/custom.json';
+import { getTargetGraph } from './network/api';
 
 const $modelerContainer = document.querySelector('#modeler-container');
 const $propertiesContainer = document.querySelector('#properties-container');
 
-import { getTargetGraph } from './network/api';
 const modeler = new Modeler({
   container: $modelerContainer,
   moddleExtensions: {
@@ -15,7 +15,6 @@ const modeler = new Modeler({
     bindTo: document.body
   }
 });
-
 
 
 getTargetGraph().then((original_record) => {
