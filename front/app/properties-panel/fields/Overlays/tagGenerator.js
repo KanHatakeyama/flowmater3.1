b86e99d9ae2605c8b8f58d8e1dffce9fab944e44
KanHatakeyama <yaj_k@msn.com>
2022-04-14 13:25:42 +0900

    smiles view

--- front/app/properties-panel/fields/Overlays/tagGenerator.js
+++ front/app/properties-panel/fields/Overlays/tagGenerator.js
@@ -16,7 +16,6 @@ export const parseLine = (item) => {
         //const title = (item.split(" ")[1]).split("_").slice(1)
         const title = (item.slice(item.indexOf("_"))).slice(1)
 
-
         let tag = '<img src=' + host_ip + 'uploaded/' + String(title)
         tag += ' alt=""></img><a id="load-graph" className="item" href='
         tag += host_ip + 'uploaded/' + String(title) + ' style={{ color: "#696969" }}target="_blank">' + item + '</a>'
@@ -27,24 +26,16 @@ export const parseLine = (item) => {
     //smiles: show chemical strucrures
     if (item.indexOf("SMILES") === 0) {
         //get pk
-        //const smiles = (item.split(" ")[1])
-        const smiles = item.slice(6)
+        const smiles = item.slice(7)
 
-        return (
-            <>
-                <div style={{ color: '#009900' }}> SMILES {smiles}</div>
-                <img src={host_ip + 'molecules/smiles/' + smiles} width={"130px"} />
-            </>
+        let tag = '<img src=' + host_ip + 'molecules/smiles/' + smiles + ' width="150 px " />'
 
-        )
+        return tag
+
 
     }
 
-    if (item.indexOf(":") > 0) {
-        return <div style={{ color: '#696969' }}> {item}</div>
-    }
 
     return ""
-    return <div>{item}</div>
 
 }
