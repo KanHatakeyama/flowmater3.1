61202848dc591d6d1ef99cfc8aefd891c621f3a1
KanHatakeyama <yaj_k@msn.com>
2022-04-19 14:02:06 +0900

    color props in react

--- front/app/properties-panel/fields/Overlays/tagGenerator.js
+++ front/app/properties-panel/fields/Overlays/tagGenerator.js
@@ -1,5 +1,4 @@
 import { host_ip } from "../../../network/api"
-import React from 'react';
 export const parseLine = (item) => {
 
     // load graph
@@ -8,35 +7,41 @@ export const parseLine = (item) => {
         //get pk
         try {
             const pk = (item.split(" ")[1]).split("_")[0]
-            return '<a id="load-graph" className="item" href="/?gid=' + String(pk) + '"style={{ color: "#FF570D" }} target="_blank">' + item + '</a>'
+            return '<NOBR><a id="load-graph" className="item" href="/?gid=' + String(pk) + 'target="_blank">' + item + '</a>' + "</NOBR><br>"
         } catch (e) { }
     }
 
 
     if (item.indexOf("file") === 0) {
-        //get pk
-        //const title = (item.split(" ")[1]).split("_").slice(1)
         const title = (item.slice(item.indexOf("_"))).slice(1)
 
-        let tag = '<img src=' + host_ip + 'uploaded/' + String(title)
+        let tag = '<NOBR><img src=' + host_ip + 'uploaded/' + String(title)
         tag += ' alt=""></img><a id="load-graph" className="item" href='
-        tag += host_ip + 'uploaded/' + String(title) + ' style={{ color: "#696969" }}target="_blank">' + item + '</a>'
-        return tag
+        tag += host_ip + 'uploaded/' + String(title) + '  target="_blank">' + item + '</a>'
+        return tag + "</NOBR><br>"
 
     }
 
     //smiles: show chemical strucrures
     if (item.indexOf("SMILES") === 0) {
-        //get pk
         const smiles = item.slice(7)
-
         let tag = '<img src=' + host_ip + 'molecules/smiles/' + smiles + ' width="150 px " />'
-
-        return tag
-
-
+        return tag + "<br>"
     }
 
+    if (item.search("=") > 0) {
+        let tag = "<NOBR>"
+        const title = item.replace(/ *=.*/, "") + " "
+        tag += '<font color="green" size=0.5% >' + title + "</font>"
+
+        const vals = item.replace(/.*= */, "")
+        tag += '<font color="gray" size=0.5%>' + vals + "</font>"
+        //let unit = vals.replace(/[0-9, \-]* /, "")
+        //let prop = vals.replace(/ *[^0-9, \-]*/, "")
+        //tag += '<font color="gray" size=0.5%>' + prop + "</font>"
+        //tag += '<font color="black" size=0.5%>' + unit + "</font>"
+        return tag + "</NOBR><br>"
+    }
 
     return ""
 
