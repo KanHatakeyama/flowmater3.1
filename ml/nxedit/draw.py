8aff2df6648e3410c55e34c6e28d552171cb7103
KanHatakeyama <yaj_k@msn.com>
2022-04-20 18:03:35 +0900

    test visualizing graphs

--- ml/nxedit/draw.py
+++ ml/nxedit/draw.py
@@ -4,7 +4,7 @@ import networkx as nx
 
 
 def save_graph_html(g: nx.DiGraph, save_path="flowchart.html", color_node_ids=[]):
-    pyvis_g = Network(directed=True)
+    pyvis_g = Network(directed=True, notebook=True)
     for i in g.nodes:
         if i in color_node_ids:
             color = '#dd4b39'
@@ -15,4 +15,4 @@ def save_graph_html(g: nx.DiGraph, save_path="flowchart.html", color_node_ids=[]
     for i, j in list(g.edges):
         pyvis_g.add_edge(i, j)
 
-    pyvis_g.save_graph(save_path)
+    return pyvis_g.save_graph(save_path)
