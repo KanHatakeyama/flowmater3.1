b9fd5b371c0025bc11942ff6f246aca4b137ea34
KanHatakeyama <yaj_k@msn.com>
2022-04-21 13:00:04 +0900

    None for numeric vals for missing va;s in FP class

--- ml/nxedit/GraphFingerprint/GraphFingerprint.py
+++ ml/nxedit/GraphFingerprint/GraphFingerprint.py
@@ -10,6 +10,7 @@ class GraphFingerprint:
 
         self.add_keys()
         self.key_list = sorted(self.key_list)
+        self._init_fp_template()
 
     def add_keys(self):
         for g in self.g_list:
@@ -21,13 +22,23 @@ class GraphFingerprint:
                 if fp_key not in self.key_list:
                     self.key_list.append(fp_key)
 
+    def _init_fp_template(self):
+        fp = {}
+        for key in self.key_list:
+            if key.startswith(NUM_CHAR):
+                fp[key] = None
+            else:
+                fp[key] = 0
+
+        self._fp_template = fp
+
     def __call__(self, g: nx.DiGraph):
         return self.calc_fingerprint(g)
 
     def calc_fingerprint(self, g: nx.DiGraph):
         fp_g = copy.deepcopy(g)
         fill_numbers(fp_g)
-        fp = {k: 0 for k in self.key_list}
+        fp = copy.deepcopy(self._fp_template)
 
         for node_id in fp_g.nodes:
             fp_key = get_fp_key(fp_g, node_id)
