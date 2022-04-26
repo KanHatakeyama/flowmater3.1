c60e3b69041c3429eb5e259f828af472b9a719bc
KanHatakeyama <yaj_k@msn.com>
2022-04-26 14:05:25 +0900

    del cache

--- back/molecules/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('smiles/<str:smiles>', views.smiles, name="smiles"),
-]
--- ml/nxedit/graph_checker/initializer.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-
-
-def claify_compounds(g: nx.DiGraph):
-    for node in g.nodes:
-        g.nodes[node]["node_name"] = g.nodes[node]["node_name"].replace(
-            "SMILES=", "Compound\nSMILES=")
